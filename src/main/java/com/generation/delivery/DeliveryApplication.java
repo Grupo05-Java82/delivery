fc40ce3cc3061435aa7e60a677f94138c8cf6282
Lucas Manrick <lucasmanrick.ipms@gmail.com>
2025-06-03 11:04:10 -0300

    :recycle: corrigindo package delivey para delivery

--- src/main/java/com/generation/delivery/DeliveryApplication.java
+++ src/main/java/com/generation/delivery/DeliveryApplication.java
@@ -1,4 +1,4 @@
-package com.generation.delivey;
+package com.generation.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
